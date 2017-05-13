b91aa11219b4a7432dbf26eb675c9fdeac49395a
Prashant Sinha <prashantsinha94@gmail.com>
2017-05-13 21:50:49 +0530

    Added problem 15

--- 15. Lattice Paths.py
+++ 15. Lattice Paths.py
@@ -1,6 +1,19 @@
 # Project Euler Problem 15
 # Lattice Paths
 
-from math import factorial
+import numpy as np
 
-print(factorial(21))
+n = 20
+
+def lattice_path_pascal(n):
+    mat = np.zeros((n+1,n+1))
+    for i in range(n+1):
+        mat[0,i] = 1
+        mat[i,0] = 1
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            mat[i,j] = mat[i-1,j] + mat[i,j-1]
+    return int(mat[n,n])
+
+print(lattice_path_pascal(n))
+    
