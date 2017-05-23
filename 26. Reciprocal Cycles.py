76801e0b66f27013dfa65fd5c9acb3e3182f9868
Prashant Sinha <prashantsinha94@gmail.com>
2017-05-23 19:50:54 +0530

    Added Problem 26

---
+++ 26. Reciprocal Cycles.py
@@ -0,0 +1,11 @@
+# Project Euler Problem 26
+# Reciprocal Cycles
+
+def recurring_cycle(n, d):
+    for t in range(1, d):
+        if 1 == 10**t % d:
+            return t
+    return 0
+
+longest = max(recurring_cycle(1, i) for i in range(2,1001))
+print([i for i in range(2,1001) if recurring_cycle(1, i) == longest][0])
